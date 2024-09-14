ea48b7d3c6d030e734314dbae600b29bd951ba6f
guusdias <gustavohd34@gmail.com>
2024-09-14 16:22:47 -0300

    fix: finished first modue

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -55,7 +55,7 @@ var job = "programmer";
 job = "teacher";
  */
 
-// math operators
+/* // math operators
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSara = now - 2018;
@@ -74,11 +74,11 @@ x += 10; // x = x + 10 = 25
 x *= 10; // x = x + 4 = 100
 x++; // x = x+1 = 101
 x--; // x = x+1 = 100
-x--; // x = x+1 = 99
+x--; // x = x+1 = 99 */
 
 //comaparison operators
 // > , < , >=, <=
-console.log(ageJonas > ageSara); // true
+/* console.log(ageJonas > ageSara); // true
 console.log(ageSara >= 18); // false
 
 const isFullAge = ageSara >= 18;
@@ -95,3 +95,110 @@ const BMIJohn = massJohn / (heightJohn * heightJohn);
 
 const markHigherBMI = BMIMark > BMIJohn;
 console.log(BMIJohn, BMIMark, markHigherBMI);
+ */
+
+// STRINGS
+
+/* const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonas);
+
+console.log("String with \n multiple \n lines");
+
+console.log(`String
+  new 
+  lines`);
+ */
+/* 
+const age = 19;
+const isOldEnough = age >= 18;
+if (isOldEnough) {
+  console.log("sarah pode dirigir");
+}
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 95;
+const heightJohn = 1.88;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+
+if (markHigherBMI) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else {
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMarko})!`);
+}
+ */
+
+/* const inputYear = "2002";
+console.log(Number(inputYear), inputYear);
+console.log(inputYear + 18);
+ */
+
+// 5 falsy values => 0, '', null, undefined, NaN
+
+/* console.log(Boolean(0)); //false
+console.log(Boolean(undefined)); //false
+console.log(Boolean("Jonas")); //true
+console.log(Boolean({})); // true
+console.log(Boolean('')); //false
+const isOldEnough = 0;
+if (isOldEnough) {
+  console.log("sarah pode dirigir");
+}else{
+	console.log('oi')
+}// 'oi'
+
+
+const heigh;
+if (heigh) {
+  console.log("sarah pode dirigir");
+}else{
+	console.log('oi')
+}// 'oi' because height is undefined */
+
+/* const age = 18;
+if (age === 18) console.log("you just became adult");
+
+"18" == 18; //true
+"18" === 18; //false
+18 === 18; //true
+18 === 19; //false
+
+18 == 18; //true --> loose operator AVOID THIS ONE
+18 === 18; //true --> strict operator
+ */
+
+/* const hasCNH = true
+const hadGoodVision = false
+console.log(hasCNH && hadGoodVision); // false
+console.log(hasCNH || hadGoodVision); // true
+console.log(!hasCNH);// false
+ */
+/* 
+const scoreDolphins = (96 + 108) / 2;
+let scoreKoalas = (88 + 91 + 110) / 3;
+
+if (scoreDolphins > scoreKoalas) {
+  console.log("Dolphins win the trophy");
+} else if (scoreDolphins > scoreKoalas) {
+  console.log("Koalas win the trophy");
+} else {
+  console.log("Both win the trophy");
+}
+ */
+
+// const age = 23
+// age>= 18 ? console.log('i like wine') : console.log('i like coffee');
+
+const bill = 275;
+let billByFifity = bill * (15 / 100);
+let billByTwenty = bill * (20 / 100);
+let tip = bill >= 500 && bill <= 300 ? billByTwenty : billByFifity;
