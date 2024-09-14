9d8ec2761d4fb3dfd65224fe8a22782fde221868
guusdias <gustavohd34@gmail.com>
2024-09-14 09:42:38 -0300

    feat: values assingments and practices

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,23 @@
+let js = "amazing";
+if (js === "amazing") alert("hellow world");
+
+
+//things you cant do
+let name = 'hey'
+let new = 'hey'
+let function = 'hey'
+
+// if you want to do that you should put other caratcters in front
+let $function
+let _function
+
+// there's also good practices
+
+//right
+let myFirstJob = 'programmer'
+let myCurrentJob = 'teacher'
+
+//wrong
+let job1 = 'programmer'
+let job2 = 'teacher'
+
