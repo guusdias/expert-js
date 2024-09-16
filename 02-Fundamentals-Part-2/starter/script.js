84556efb121f048508a9f94bc7c9e638b1e1c667
guusdias <gustavohd34@gmail.com>
2024-09-16 15:58:18 -0300

    feat: functions

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,35 @@
+//strict allows us to have a secure, avoiding bugs, code in javascript
+"use strict";
+
+/* function logger() {
+  console.log("My name is Gustavo");
+}
+// calling/running / invoking the function
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice); //Juice with 5 apples and 0 oranges
+
+const appleOragneJuice = fruitProcessor(2, 4);
+console.log(appleOragneJuice); //Juice with 2 apples and 4 oranges
+ */
+
+/* // function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+// anonymous function or function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1991);
+console.log(age1, age2); */
