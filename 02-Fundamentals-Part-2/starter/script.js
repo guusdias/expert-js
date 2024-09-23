66ecd45d5863561d626863b9462bcdc42c1bae37
guusdias <gustavohd34@gmail.com>
2024-09-23 17:40:36 -0300

    finish funadamental

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -169,6 +169,16 @@ const jonas = {
 };
 
 // o array começa a contar no 0
-for (let i = 0; i >= jonas.length; i++) {
-  console.log(jonas[i]);
+console.log("--- ONLY STRINGS ---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== "string") continue;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log("--- BREAK WITH NUMBER ---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === "number") break;
+
+  console.log(jonas[i], typeof jonas[i]);
 }
